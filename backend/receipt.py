a5cfe1434e460e43b480e96e164b0701fab282bc
Austin <austin.vuong00@gmail.com>
2019-07-27 17:56:04 +1000

    Receipt working in progress

--- backend/receipt.py
+++ backend/receipt.py
@@ -1,21 +1,29 @@
 from datetime import datetime
+from item import Item
+# import json_operations
+import json
 class Receipt():
     def __init__(self):
         self._items = []
         self._name = ""
-        self._date = date
+        self._date = datetime
         self._category = ""
         self._id = ""
+        self._store = ""
 
-    def createReceipt(self):
-        f = open("sample.txt", "r")
-        
+    def createReceipt(self, file):
+        with open(file, 'r') as f:
+            data = json.load(f)
+            self._store = data.get("store")
+            self._date = datetime.strptime(data.get("date"), '%b %d %Y')
+
+            self._category = data.get("category")
+            tempItems = data.get("items")
+            for it in tempItems:
+                new = Item(it.get("name"), it.get("quantity"), it.get("price"), self._store)
+                self._items.append(new)
+                # print(new)
 
 
-
-
-
-
-
-    def addItem(self, item):
-        self._items.append(item)
+# r = Receipt()
+# r.createReceipt('sample2.json')
