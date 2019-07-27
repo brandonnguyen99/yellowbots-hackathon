6217712df7e5ef25e8efb907ad28e2e05b4081a3
Cindy Zhou <z5209956@ad.unsw.edu.au>
2019-07-27 19:51:13 +1000

    finished home page

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -6,3 +6,7 @@ blueprint = Blueprint('home', __name__)
 @blueprint.route('/')
 def index():
     return render_template('home/index.html')
+
+@blueprint.route('/404')
+def error():
+    return render_template('404.html')
