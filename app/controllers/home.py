80a4c6ee57c98cbc4a0348efda486bf51fd616bf
Cindy Zhou <z5209956@ad.unsw.edu.au>
2019-07-27 21:20:17 +1000

    added layout and other webpaged

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -10,3 +10,15 @@ def index():
 @blueprint.route('/404')
 def error():
     return render_template('404.html')
+
+@blueprint.route('/company_receipt/<company>')
+def company_receipt(company):
+    return render_template('home/company_receipt.html', company = company);
+
+@blueprint.route('/add_receipt')
+def add_receipt():
+    return render_template('home/add_receipt.html');
+
+@blueprint.route('/main')
+def main():
+    return render_template('home/main.html');
