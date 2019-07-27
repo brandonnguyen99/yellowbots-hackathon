aa6007eb14ce32df5d5182520b65e7aba1a570d0
Austin <austin.vuong00@gmail.com>
2019-07-27 14:27:58 +1000

    Initial implementations of receipt.py

---
+++ backend/receipt.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+class Receipt():
+    def __init__(self):
+        self._items = []
+        self._name = ""
+        self._date = date
+        self._category = ""
+        self._id = ""
+
+    def createReceipt(self):
+        f = open("sample.txt", "r")
+        
+
+
+
+
+
+
+
+    def addItem(self, item):
+        self._items.append(item)
