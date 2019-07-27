37cc6f3dcd5ab507e4aaf78b19a3428d8a1df208
winstondarmawan <winstonadarmawan@gmail.com>
2019-07-27 14:39:09 +1000

    finished user.py

---
+++ backend/user.py
@@ -0,0 +1,35 @@
+from receipt import Receipt
+
+class User()
+    def __init__(self, id, password):
+        self._id = id
+        self._receipts = []
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def total_receipts(self):
+        return self._receipts
+
+    @property
+    def num_receipts(self):
+        return len(self._receipts)
+
+    @receipts.setter
+    def addReceipt(self, newReceipt):
+        self._receipts.append(newReceipt)
+
+    def retrieveCategory(self, category):
+        category_receipts = []
+        for receipt in self._receipts:
+            if (receipt.category == category):
+                category_receipts.append(receipt)
+        
+        return category_receipts
+
+
+    def __str__(self):
+        return "id: {}, Receipts: {}".format(self._id, self._receipts)
+
